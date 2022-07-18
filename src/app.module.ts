36fc1dacc1bfed56626ac82a9786d0f657badf80
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2022-07-18 10:06:50 -0300

    project files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,7 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    UsersModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://root:654123@crud-nestjs.muqap.mongodb.net/test',
-    ),
-  ],
+  imports: [UsersModule, MongooseModule.forRoot(process.env.DB_URI)],
   controllers: [],
   providers: [],
 })
